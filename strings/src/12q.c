3abc8a196acc6ac3ed55005642e8b0b9a53c26a5
gabrielfruet <gabrielfruet538@gmail.com>
2022-06-09 20:22:54 -0300

    refactor: modifying q12 from strings list

--- strings/src/12q.c
+++ strings/src/12q.c
@@ -5,11 +5,11 @@
 #include <time.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <math.h>
 #include <stdbool.h>
 
 
-void numeroPorExtenso(char* dest, const char* src){
-  unsigned int num = atoi(src);
+void trioPorExtenso(char* dest, unsigned int num){
   const unsigned int num_ORIGINAL = num;
   char* unidades[] = {
     "um",
@@ -45,7 +45,7 @@ void numeroPorExtenso(char* dest, const char* src){
     "noventa"
   };
   char* centenas[] = {
-    "cem",
+    "cento",
     "duzentos",
     "trezentos",
     "quatrocentos",
@@ -56,50 +56,71 @@ void numeroPorExtenso(char* dest, const char* src){
     "novecentos",
   };
   bool precisa_de_e = false;
-  if(num >= 1000){
-    int vezes = num/1000;     
-    if(vezes > 1){
-      strcat(dest, unidades[vezes - 1]);      
-      strcat(dest, " mil");
-    }else{
-      strcat(dest, "mil");
-    }
-    precisa_de_e = true;
-    num %= 1000;
-  }
   if(num >= 100){
     int vezes = num/100;
-    strcat(dest, precisa_de_e ? " e " : num_ORIGINAL > 999 ? " " : "");
-    strcat(dest, centenas[vezes - 1]);
+    strcat(dest, num == 100 ? "cem" : centenas[vezes - 1]);
     precisa_de_e = true;
     num %= 100;
   }
   if(num >= 10){
     int vezes = num/10; 
-    if(vezes > 1){
-      strcat(dest, precisa_de_e ? " e " : num_ORIGINAL > 99 ? " " : "");
+    if(vezes >= 2){
+      strcat(dest, precisa_de_e ? " e " : num_ORIGINAL >= 100 ? " " : "");
       strcat(dest, dezenas[vezes - 2]);
       num %= 10;
       precisa_de_e = true;
     } 
   }
   if(num > 0){
-    strcat(dest, precisa_de_e ? " e " : num_ORIGINAL > 99 ? " " : "");
+    strcat(dest, precisa_de_e ? " e " : num_ORIGINAL >= 100 ? " " : "");
     if(num > 9){
       strcat(dest, dez_a_dezenove[num - 10]);
     }else{
       strcat(dest, unidades[num - 1]);
     }
   }
+}
+
+
+void numeroPorExtenso(char* dest, const char* src){
+  unsigned int num = atoi(src);
+  const unsigned int num_ORG = num;
+
+  char* classes[5][2] = {
+    {"mil"},
+    {"milhao", "milhoes"},
+    {"bilhao", "bilhoes"},
+    {"trilhao", "trilhoes"},
+    {"quadrilhao", "quadrilhao"}
+  };
+  bool precisa_de_separador = false;
+  unsigned int trio = 1;
+
+  for(int exp = 15; exp >= 0; exp -= 3){
+    trio = num/pow(10,exp);
+    num = num % (int)pow(10,exp);
+    if(trio){
+      if(precisa_de_separador)
+        strcat(dest, exp == 0 ? " e " : ", ");
+
+      trioPorExtenso(dest, trio);
+      if(exp > 0){
+        strcat(dest, " ");
+        strcat(dest, classes[exp/3 - 1][trio > 1 && exp != 3]);
+      }
+      precisa_de_separador = true; 
+    }
+  }
   dest[0] = toupper(dest[0]);
 }
 
 int main(){
-  char ano[10];
+  char ano[11];
   printf("Digite um ano para descobrir por extenso: ");
-  fgets(ano, 9, stdin);
-  char ano_extenso[150] = "";
+  fgets(ano, 10, stdin);
+  char ano_extenso[151] = "";
   numeroPorExtenso(ano_extenso, ano);
   printf("ANO POR EXTENSO: %s anos\n", ano_extenso);
-  return 0;
+
+  return 1;
 }
